7bc5f402f11d57c6a82018015cbd53cddfa2915c
Alex Bisztray <alex@engineeredcode.com>
2023-03-04 15:58:31 -0500

    changing default video. probably gonna redesign altogether.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,10 @@
 let curr = 1;
 function changeBg() {
     let vids = [
+        "assets/videos/water.mp4",
         "assets/videos/city-snow.m4v",
         "assets/videos/evening-traffic.m4v",
         "assets/videos/train.m4v",
-        "assets/videos/forest.mp4",
-        "assets/videos/window-rain.m4v",
     ];
     let source = document.getElementById("bg-video");
     source.src = vids[curr];
